eccecc51e4c8dc288980910886e678f32c6d1e4f
obreira <42821882+obreira@users.noreply.github.com>
2023-05-30 21:41:38 +0100

    Initial commit

---
+++ src/app/store.js
@@ -0,0 +1,20 @@
+import { configureStore } from "@reduxjs/toolkit";
+import topicsReducer from "../features/topics/topicsSlice";
+import quizzesReducer from "../features/quizzes/quizzesSlice.js";
+import cardsReducer from "../features/cards/cardsSlice";
+
+
+
+
+export default configureStore({
+
+
+  reducer: {
+    topics: topicsReducer,
+    quizzes: quizzesReducer,
+    cards: cardsReducer,
+
+  },
+
+
+});
